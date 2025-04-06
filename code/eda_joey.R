0de26e2b4c961c8eca7b2a3f3062d6487f4f48f5
jannand09 <josephrannand@gmail.com>
2025-04-06 13:34:07 -0400

    Update eda_joey.R

--- code/eda_joey.R
+++ code/eda_joey.R
@@ -18,6 +18,35 @@ names(csv_list) <- tools::file_path_sans_ext(basename(all_csv))
 
 ###############################################################################
 
+# create abbreviations for which csv file each column in full dataset came from
+abbreviations <- c("ab","ap","tf","md","pvb","pvp","sbm","sb","sp")
+
+# Remove index rows with no column name
+for (i in 1:length(csv_list)) {
+  working_csv <- csv_list[[i]]
+  working_csv <- working_csv[,names(working_csv) != ""]
+  csv_list[[i]] <- working_csv
+}
+
+# Add abbreviation to columns in each csv
+for (i in 1:length(csv_list)) {
+  working_csv <- csv_list[[i]]
+  for (col in colnames(working_csv)) {
+    # do not change the Tm and year columns so that full_join can be performed
+    if (col != "Tm" & col != "Year") {
+      new_col <- paste(abbreviations[i],col,sep = ".")
+      names(working_csv)[names(working_csv) == col] <- new_col
+    }
+  }
+  csv_list[[i]] <- working_csv
+}
+
+
+################################################################################
+
+
 full_dataset <- csv_list %>% reduce(full_join, by =c("Tm","Year"))
 
 colnames(full_dataset)
+
+write.csv(full_dataset, paste(dataset_folder,"/all_data.csv", sep=""))
