b5330219ab146ca90ec73713d40e0352fa9e5885
jannand09 <josephrannand@gmail.com>
2025-04-07 22:56:36 -0400

    Update eda

--- code/eda_joey.R
+++ code/eda_joey.R
@@ -9,6 +9,7 @@ library(readr)
 library(corrplot)
 library(ggcorrplot)
 library(caret)
+library(moments)
 
 dataset_folder <- paste(getwd(),"/final_data",sep="")
 
@@ -57,6 +58,42 @@ pca_cols <- c("sbm.RC","sb.TB","sb.R","sb.RBI","sb.H","sb.LOB","pvb.PA","sp.L","
 
 cor_matrix <- cor(cor_data %>% select(all_of(pca_cols)), use = "pairwise.complete.obs")
 
+pca_corrplot <- corrplot(cor_matrix, method = "color", tl.cex = 0.6, tl.col = "black")
+
+pca_cols <- c("Tm","Year","Team.Success",pca_cols)
+pca_data <- cor_data %>% select(all_of(pca_cols))
+
+################################################################################
+
+freq_table <- table(pca_data$Team.Success)                # Frequency count
+percent_table <- prop.table(freq_table) * 100  # Convert to percentages
+
+# Combine into a single data frame
+summary_table <- data.frame(
+  Team.Success = names(freq_table),
+  Frequency = as.vector(freq_table),
+  Percentage = round(as.vector(percent_table), 2)
+)
+
+print(summary_table)
+
+write_csv(summary_table, paste(getwd(),"/images/summary_table.csv", sep=""))
+
+# Example: response is your response variable (factor or integer), and df is your data frame
+summary_stats <- pca_data %>%
+  group_by(Team.Success) %>%
+  summarise(across(where(is.numeric), list(
+    mean = ~mean(.x, na.rm = TRUE),
+    median = ~median(.x, na.rm = TRUE),
+    sd = ~sd(.x, na.rm = TRUE),
+    iqr = ~IQR(.x, na.rm = TRUE),
+    skew = ~skewness(.x, na.rm = TRUE),
+    kurt = ~kurtosis(.x, na.rm = TRUE)
+  ), .names = "{.col}_{.fn}")) %>%
+  pivot_longer(-Team.Success, names_to = c("Variable", "Statistic"), names_sep = "_") %>%
+  pivot_wider(names_from = Statistic, values_from = value)
+
+write.csv(summary_stats, paste(getwd(),"/images/summary_stats.csv", sep=""))
 
 ############## Debugging standard deviation is zero error from cor() ###########
 
